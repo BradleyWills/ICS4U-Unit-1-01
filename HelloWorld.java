26767257c0394db3fb7c3ce3675900cff6145284
Bradley Wills <bradley.wills@mths.ca>
2020-02-11 18:08:01 +0000

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Bradley Wills
+ * 2020-01-11
+ * Class HelloWorld.
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+    	System.out.println("Hello from AWS Cloud9!");
+	}
+}
